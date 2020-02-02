c98b52bfa1eedaf2f9e87071d710509f5d50840f
İbrahim Sanduvaç <ihsanduvac@gmail.com>
2020-02-02 17:37:08 +0300

    release

---
+++ README.md
@@ -0,0 +1,148 @@
+# Crazy Primes (Useful Prime Numbers Functions)
+
+![](https://dev.truncgil.com/prime-logo-180x180.png)
+
+Primes are of the utmost importance to number theorists because they are the building blocks of whole numbers, and important to the world because their odd mathematical properties make them perfect for our current uses.
+On that matter we've built a library to create and find prime numbers
+
+### Features
+
+- Basic prime number generators
+- Primes' indexes
+- High performance
+- Some special prime arrays
+- Relations with normal integers
+
+## Installation
+<pre>npm install crazy-primes</pre>
+## Usage
+
+```javascript
+const pr = require('crazy-primes');
+```
+
+## Functions
+#### isPrime(number)
+```javascript
+let result = pr.isPrime(13);    // true
+```
+```javascript
+let result = pr.isPrime(28);    // false
+```
+#### nthPrime(order)
+```javascript
+let result = pr.nthPrime(5);    // 11
+```
+#### indexOfPrime(primeNumber)
+```javascript
+let result = pr.indexOfPrime(13);    // 5
+```
+Index starts from 0
+#### nthPrimesSum(...arguments)
+```javascript
+let result = pr.nthPrimesSum(3,5,7);    // 5 + 11 + 17 = 33
+```
+#### nthPrimesTimes(...arguments)
+```javascript
+let result = pr.nthPrimesTimes(3,5,7);    // 5 * 11 * 17 = 935
+```
+#### nextPrime(currentPrime)
+```javascript
+let result = pr.nextPrime(17);    // 19
+```
+#### prevPrime(currentPrime)
+```javascript
+let result = pr.prevPrime(17);    // 13
+```
+#### primeSmallerThan(number)
+```javascript
+let result = pr.primeSmallerThan(100);    // 97
+```
+#### primeBiggerThan(number)
+```javascript
+let result = pr.primeBiggerThan(100);    // 101
+```
+#### primeDivisors(nonPrimeNumber)
+```javascript
+let result = pr.primeDivisors(42);    // [2,3,7]
+```
+#### primeDivisorsSum(nonPrimeNumber)
+```javascript
+let result = pr.primeDivisorsSum(42);    // 2 + 3 + 7 = 12
+```
+#### primeDivisorsTimes(nonPrimeNumber)
+```javascript
+let result = pr.primeDivisorsTimes(42);    // 2 * 3 * 7 = 42
+```
+#### isMersennePrime(primeNumber)
+```javascript
+let result = pr.isMersennePrime(127);    // true
+```
+#### nthMersennePrime(order)
+```javascript
+let result = pr.nthMersennePrime(5);    // 8191
+```
+#### nthMersennePrimeExponents(order)
+```javascript
+let result = pr.nthMersennePrimeExponents(5);    // 13  - That means 2^13
+```
+#### isPrimeOrDivisors(number)
+If the number is prime it returns true, otherwise it returns prime divisors
+#### primesSmallerThan(number)
+```javascript
+let result = pr.primesSmallerThan(25);    // [ 2, 3, 5, 7, 11, 13, 17, 19, 23 ]
+```
+#### closestPrime(number)
+```javascript
+let result = pr.closestPrime(25);    // 23
+```
+#### randomPrime(minVal, maxVal)
+```javascript
+let result = pr.randomPrime(25, 48);    // 31
+```
+#### whatWillThisPrimeBe(primeNumber)
+```javascript
+let result = pr.whatWillThisPrimeBe(23);    // It'll strengthen you
+```
+#### nextNPrimes(minVal, n)
+```javascript
+let result = pr.nextNPrimes(25, 5);    // [ 29, 31, 37, 41, 43 ]
+```
+#### prevNPrimes(number)
+```javascript
+let result = pr.prevNPrimes(25, 5);    // [ 23, 19, 17, 13, 11 ]
+```
+#### primesBetween(number1, number2)
+```javascript
+let result = pr.primesBetween(80, 150);    // [ 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149 ]
+```
+#### firstNPrimes(count)
+```javascript
+let result = pr.firstNPrimes(7);    // [ 2, 3, 5, 7, 11, 13, 17 ]
+```
+#### digits(number)
+helper function
+```javascript
+let result = pr.digits(1554);    // 4
+```
+#### sum(numbersArray)
+helper function
+```javascript
+let result = pr.sum([2,3,4]);    // 9
+```
+#### times(numbersArray)
+helper function
+```javascript
+let result = pr.times([2,3,4]);    // 24
+```
+#### remainDividedBy(number, divisor)
+helper function
+```javascript
+let result = pr.remainDividedBy(8,3);    // 2
+```
+#### printExecutionTime()
+helper function
+That should be bottom of the script
+```javascript
+pr.printExecutionTime();    // Execution time: 119ms
+```
---
+++ index.js
@@ -0,0 +1,436 @@
+let start = new Date();
+let simulateTime = 5;
+
+exports.printExecutionTime = () => {
+    setTimeout(function (argument) {
+        let end = new Date() - start;
+        console.info('Execution time: %dms', end)
+    }, simulateTime)
+}
+
+exports.isPrime = (val) => {
+    if (val == 1)
+        return false;
+    else if (val == 2)
+        return true;
+    else if (val % 2 == 0)
+        return false;
+    else if (val > 3 && val % 3 == 0 || val > 5 && val % 5 == 0 || val > 7 && val % 7 == 0 || val > 11 && val % 11 == 0 || val > 13 && val % 13 == 0 || val > 17 && val % 17 == 0 || val > 19 && val % 19 == 0)
+        return false;
+    else {
+        let res = true;
+        for (var i = 3; i <= Math.round(Math.sqrt(val)); i += 2) {
+            if (val % i == 0) {
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
+}
+
+exports.isPrimeOld = (val) => {
+    res = true;
+    for (let i = 2; i < val; i++) {
+        if (val % i == 0) {
+            res = false;
+            break;
+        }
+    }
+    return res;
+}
+
+exports.nthPrime = (val, maxIterator = 99999999999) => {
+    let counter = 1;
+    if (val == 1) {
+        return 2;
+    } else {
+        var res = false;
+        for (var i = 3; i <= maxIterator; i += 2) {
+            if (this.isPrime(i)) {
+                counter += 1;
+                if (counter == val) {
+                    res = i;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+exports.indexOfPrime = (val) => { // 0 is first index
+    if (!this.isPrime(val))
+        return false;
+    else {
+        var i = 1;
+        var res;
+        while (true) {
+            if (this.nthPrime(i) == val) {
+                res = i;
+                break;
+            }
+            i++;
+        }
+        return res - 1;
+    }
+}
+
+exports.nthPrimesSum = (...args) => {
+    var sum = 0;
+    for (var i = 0; i < args.length; i++) {
+        sum += this.nthPrime(args[i]);
+    }
+    return sum;
+}
+
+exports.nthPrimesTimes = (...args) => {
+    var times = 1;
+    for (var i = 0; i < args.length; i++) {
+        times *= this.nthPrime(args[i]);
+    }
+    return times;
+}
+
+exports.nextPrime = (val) => {
+    if (!this.isPrime(val))
+        return false;
+    else {
+        var counter = 1;
+        var stopCounter;
+        while (1 == 1) {
+            var currPrime = this.nthPrime(counter);
+            if (currPrime == val) {
+                stopCounter = counter;
+                break;
+            } else
+                counter += 1;
+        }
+        return this.nthPrime(stopCounter + 1);
+    }
+}
+
+exports.prevPrime = (val) => {
+    if (!this.isPrime(val) || val == 2)
+        return false;
+    else {
+        var counter = 1;
+        var stopCounter;
+        while (1 == 1) {
+            var currPrime = this.nthPrime(counter);
+            if (currPrime == val) {
+                stopCounter = counter;
+                break;
+            } else
+                counter += 1;
+        }
+        return this.nthPrime(stopCounter - 1);
+    }
+}
+
+exports.primeSmallerThan = (val) => {
+    if (this.isPrime(val)) {
+        return this.prevPrime(val);
+    } else {
+        var i = 1;
+        var res;
+        while (1 == 1) {
+            if (val < this.nthPrime(i + 1) && val > this.nthPrime(i)) {
+                res = this.nthPrime(i);
+                break;
+            }
+            i += 1;
+        }
+        return res;
+    }
+}
+
+exports.primeBiggerThan = (val) => {
+    if (this.isPrime(val))
+        return this.nextPrime(val);
+    else {
+        var i = 1;
+        var res;
+        while (1 == 1) {
+            if (val > this.nthPrime(i) && val < this.nthPrime(i + 1)) {
+                res = this.nthPrime(i + 1);
+                break;
+            }
+            i += 1;
+        }
+        return res;
+    }
+}
+
+exports.primeDivisors = (val) => {
+    if (this.isPrime(val))
+        return false; //Prime
+    else {
+        var arr = [];
+        if (val % 2 == 0)
+            arr.push(2);
+        for (var i = 3; i < val; i += 2) {
+            if (this.isPrime(i) && val % i == 0)
+                arr.push(i);
+        }
+        return arr;
+    }
+}
+
+exports.primeDivisorsSum = (val) => {
+    if (this.isPrime(val))
+        return false;
+    else {
+        var pD = this.primeDivisors(val);
+        var res = 0;
+        for (let i = 0; i < pD.length; i++) {
+            res += pD[i];
+        }
+        return res;
+    }
+}
+
+exports.primeDivisorsTimes = (val) => {
+    if (this.isPrime(val))
+        return false;
+    else {
+        var pD = this.primeDivisors(val);
+        var res = 1;
+        for (let i = 0; i < pD.length; i++) {
+            res *= pD[i];
+        }
+        return res;
+    }
+}
+
+exports.isMersennePrime = (val) => {
+    if (!this.isPrime(val))
+        return false;
+    else {
+        val = val + 1;
+        let primeDiv = this.primeDivisors(val);
+        if (primeDiv.length == 1 && primeDiv[0] === 2)
+            return true;
+        else
+            return false;
+    }
+}
+
+exports.nthMersennePrime = (val, maxIterator = 99999999999) => { // 0 is first
+    let counter = 0;
+    let res = false;
+    for (let i = 1; i < maxIterator; i++) {
+        let curr = Math.pow(2, i) - 1;
+        if (this.isPrime(curr)) {
+            counter += 1;
+            if (counter == val) {
+                res = curr;
+                break;
+            }
+        }
+
+    }
+    return res;
+}
+
+exports.nthMersennePrimeExponents = (val, maxIterator = 99999999999) => {
+    let mersenne = this.nthMersennePrime(val, maxIterator);
+    mersenne = mersenne + 1;
+    let i = 0;
+    let stop = false;
+    let ret = false;
+    while (stop == false) {
+        i += 1;
+        if (mersenne / 2 == 1) {
+            ret = i;
+            stop = true;
+            break;
+        } else {
+            mersenne = mersenne / 2;
+        }
+    }
+
+    return ret;
+}
+
+exports.isPrimeOrDivisors = (val) => {
+    if (this.isPrime(val))
+        return true;
+    else
+        return this.primeDivisors(val);
+}
+
+exports.primesSmallerThan = (val) => {
+    var i = 1;
+    var res = [];
+    while (1 == 1) {
+        res.push(this.nthPrime(i));
+        if (val < this.nthPrime(i + 1) && val > this.nthPrime(i)) {
+            break;
+        }
+        i += 1;
+    }
+    return res;
+}
+
+exports.closestPrime = (val) => {
+    let bigger = false;
+    let smaller = false;
+    for (let i = val + 1; i < Math.pow(val, 3); i++) {
+        if (this.isPrime(i)) {
+            bigger = i;
+            break;
+        }
+    }
+    for (let j = val - 1; j > 1; j--) {
+        if (this.isPrime(j)) {
+            smaller = j;
+            break;
+        }
+    }
+    let res;
+    if (!bigger)
+        res = smaller;
+    else if (!smaller)
+        res = bigger;
+    else if (bigger - val == val - smaller) {
+        res = bigger;
+    } else if (bigger - val < val - smaller) {
+        res = bigger;
+    } else
+        res = smaller;
+    return res;
+}
+
+exports.randomPrime = (minVal = 2, maxVal = 9999999999999999) => {
+    let rnd = Math.floor(Math.random() * (maxVal - minVal)) + minVal;
+    rnd = this.closestPrime(rnd);
+    return rnd;
+}
+
+exports.randomPrimeDigits = (digit) => {
+    let a = "1";
+    let b = "9";
+    for (let i = 0; i < digit; i++) {
+        a += "0";
+        b += "9";
+    }
+
+    a = parseInt(a);
+    b = parseInt(b);
+    let prime = this.randomPrime(a, b);
+    return prime;
+
+}
+
+exports.whatWillThisPrimeBe = (val) => {
+    if (this.isPrime(val)) {
+        let arr = [
+            "It'll communicate with you",
+            "It'll be lucky for you",
+            "It'll strengthen you",
+            "It'll make you happy"
+        ];
+        var kal = val % arr.length;
+        return arr[kal - 1];
+    } else
+        return false;
+}
+
+exports.nextNPrimes = (minVal, n) => {
+    let primes = [];
+    let it;
+    for (var i = 0; i < n; i++) {
+        if (i == 0) {
+            it = this.primeBiggerThan(minVal);
+        } else {
+            it = this.nextPrime(it);
+        }
+        primes.push(it);
+    }
+    return primes;
+}
+
+exports.prevNPrimes = (maxVal, n) => {
+    let primes = [];
+    let it;
+    for (var i = n; i > 0; i--) {
+        if (i == n) {
+            it = this.primeSmallerThan(maxVal);
+        } else {
+            it = this.prevPrime(it);
+        }
+        primes.push(it);
+    }
+    return primes;
+}
+
+exports.primesBetween = (p1, p2) => {
+    let check = true;
+    let start;
+    let finish;
+    if (p1 > p2) {
+        start = p2;
+        finish = p1;
+    } else if (p2 > p1) {
+        start = p1;
+        finish = p2;
+    } else {
+        check = false;
+    }
+    if (check) {
+        let res = [];
+        let first = this.primeBiggerThan(start);
+        res.push(first);
+        let contin = true;
+        while (contin) {
+            first = this.nextPrime(first);
+            if (first >= finish) {
+                contin = false;
+                break;
+            } else {
+                res.push(first);
+            }
+        }
+        return res;
+    } else
+        return false;
+}
+
+exports.firstNPrimes = (n) => {
+    if (n <= 0)
+        return false;
+    else {
+        let primes = [];
+        let next = 2;
+        for (i = 1; i <= n; i++) {
+            primes.push(next);
+            next = this.nextPrime(next);
+        }
+        return primes;
+    }
+}
+
+exports.digits = (val) => {
+    return String(val).length;
+}
+
+exports.sum = (arr) => {
+    let res = 0;
+    for (let i = 0; i < arr.length; i++) {
+        res += arr[i];
+    }
+    return res;
+}
+
+exports.times = (arr) => {
+    let res = 1;
+    for (let i = 0; i < arr.length; i++) {
+        res *= arr[i];
+    }
+    return res;
+}
+
+exports.remainDividedBy = (number, division) => {
+    return number % division;
+}
